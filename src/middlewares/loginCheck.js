908b3da7af675dc5fca47ab56a7c387992a9de17
tiantian <1500846601@qq.com>
2020-07-16 09:14:49 +0800

    feat: 抽离登录验证中间件

---
+++ src/middlewares/loginCheck.js
@@ -0,0 +1,26 @@
+const {ErrorModel} = require('../model/ResModel')
+const {loginCheckFailInfo} = require('../model/ErrorInfo')
+
+const loginCheck = async (ctx, next) => {
+  if (ctx.session && ctx.session.userInfo) {
+    await next()
+    return
+  }
+
+  ctx.body = new ErrorModel(loginCheckFailInfo)
+}
+
+
+const loginRedirect = async (ctx, next) => {
+  if (ctx.session && ctx.session.userInfo) {
+    await next()
+    return
+  }
+
+  const curUrl = ctx.url
+  ctx.redirect(`/login?url=${encodeURIComponent(curUrl)}`)
+}
+
+module.exports = {
+  loginCheck
+}
