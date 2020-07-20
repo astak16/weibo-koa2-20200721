56c92a7ca22bf1cc234621ed94e49b91c08caf8e
tiantian <1500846601@qq.com>
2020-07-20 18:05:51 +0800

    feat: 用户设置，单元测试

--- test/user/login.test.js
+++ test/user/login.test.js
@@ -47,7 +47,36 @@ test('登录，应该成功', async () => {
   expect(res.body.errno).toBe(0)
 
   COOKIE = res.headers['set-cookie'].join(';')
+})
 
+test('修改基本信息应该成功', async () => {
+  const res = await server
+    .patch('/api/user/changeInfo')
+    .send({
+      nickName: '测试昵称',
+      city: '测试城市',
+      picture: '/test.png'
+    })
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+})
+
+test('修改密码，应该成功', async () => {
+  const res = await server
+    .patch('/api/user/changePassword')
+    .send({
+      password,
+      newPassword: `p_${Date.now()}`
+    })
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+})
+
+test('退出登录应该成功', async () => {
+  const res = await server
+    .post('/api/user/logout')
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
 })
 
 test('删除用户，应该成功', async () => {
